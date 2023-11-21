d58f8b7e534cc9ff5d3df3c5219d945c68166bcb
ivanin3 <ivaninmenendez@gmail.com>
2023-11-21 18:41:26 +0100

    Completed

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const port = 3000
 const books = require ('./data/books.json');
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-
-
-
 
 app.get('/all', (req, res) => {
   res.send(books);
@@ -30,7 +24,35 @@ app.get('/middle', (req, res) => {
 
 app.get('/author/dante-alighieri', (req, res) => {
     const author = books.filter(el => el.author === 'Dante Alighieri');
-    res.send(author);
+    res.json(author[0].title);
+})
+
+app.get('/country/charles-dickens', (req, res) => {
+    const author = books.filter(el => el.author === 'Charles Dickens');
+    res.json(author[0].country);
+})
+
+app.get('/year&pages/cervantes', (req, res) => {
+    const author = books.filter(el => el.author === 'Miguel de Cervantes');
+    res.json({ year: author[0].year, pages: author[0].pages });
+})
+
+app.get('/country/count/spain', (req, res) => {
+    const spainBooks = books.filter(el => el.country === 'Spain');
+    res.json(spainBooks.length)
+})
+
+app.get('/country/at-least/germany', (req, res) => {
+    const result = books.some(el => el.country === 'Germany');
+    res.json(result)
+})
+
+app.get('/pages/all-greater/200', (req, res) => {
+    const result = books.every(el => el.pages > 100);
+    res.json(result)
 })
 
 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
